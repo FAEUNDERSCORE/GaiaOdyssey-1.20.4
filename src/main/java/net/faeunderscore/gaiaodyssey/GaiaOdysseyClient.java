3a3c7f2d8cee5dd09a65b271194f403f75725fe9
Cyrille PECNIK <cyrille.pecnik@iut-dijon.u-bourgogne.fr>
2024-04-22 22:25:25 +0200

    Initial commit

---
+++ src/main/java/net/faeunderscore/gaiaodyssey/GaiaOdysseyClient.java
@@ -0,0 +1,10 @@
+package net.faeunderscore.gaiaodyssey;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GaiaOdysseyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/faeunderscore/gaiaodyssey/GaiaOdysseyDataGenerator.java
@@ -0,0 +1,11 @@
+package net.faeunderscore.gaiaodyssey;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GaiaOdysseyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
