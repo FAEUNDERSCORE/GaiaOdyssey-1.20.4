f8a26414f1398bf29bd1f8caa96e31a0878f5ece
SoraxDubbing <sorax5.r23@gmail.com>
2024-11-27 21:43:18 +0100

    Hammer item + armor item render + item from block

--- src/main/java/net/faeunderscore/gaiaodyssey/renderer/item/CrystalHammerRenderer.java
+++ src/main/java/net/faeunderscore/gaiaodyssey/renderer/item/CrystalHammerRenderer.java
@@ -5,9 +5,11 @@ import net.faeunderscore.gaiaodyssey.item.custom.CrystalHammerItem;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class CrystalHammerRenderer extends GeoItemRenderer<CrystalHammerItem> {
     public CrystalHammerRenderer() {
         super(new DefaultedItemGeoModel<>(Identifier.of(GaiaOdyssey.MOD_ID, "crystal_hammer")));
+        addRenderLayer(new AutoGlowingGeoLayer<>(this));
     }
 }
